fb41a33a564f80e6adbcb8614b76a3b7eb44c11c
Asad <syedasadrazarizvi1@gmail.com>
2025-10-06 20:45:38 +1100

    Working on camera service

---
+++ src/perception_pkg/src/service_handler.cpp
@@ -0,0 +1,29 @@
+#include "rclcpp/rclcpp.hpp"
+#include "custom_msgs/srv/start_perception.hpp"
+
+#include <memory>
+
+void start_perception(const std::shared_ptr<custom_msgs::srv::StartPerception::Request> request,
+                      std::shared_ptr<custom_msgs::srv::StartPerception::Response> response)
+{
+    response->success = true;
+    response->message = "Perception started";
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request: start=%d",
+                request->start);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%d]", response->success);
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("start_perception_server");
+
+    rclcpp::Service<custom_msgs::srv::StartPerception>::SharedPtr service =
+        node->create_service<custom_msgs::srv::StartPerception>("start_perception", &start_perception);
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to start perception.");
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+}
